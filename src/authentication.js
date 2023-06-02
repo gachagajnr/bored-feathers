07f53bc27c9f4cb47e6d7b7257ea84ea0839fa26
gachagajnr <williampius17@users.noreply.github.com>
2023-06-02 19:48:16 +0300

    user can login with with google auth

--- src/authentication.js
+++ src/authentication.js
@@ -3,12 +3,32 @@ import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication'
 import { LocalStrategy } from '@feathersjs/authentication-local'
 import { oauth, OAuthStrategy } from '@feathersjs/authentication-oauth'
 
+
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    // this will set 'googleId'
+    const baseData = await super.getEntityData(profile)
+
+    // this will grab the picture and email address of the Google profile
+    console.log(profile)
+    const [fname, lname]=profile.name.split(' ')
+    return {
+      ...baseData,
+      firstName:fname,
+      lastName:lname,
+      avatar: profile.picture,
+      email: profile.email,
+      phoneNumber:''
+    }
+  }
+}
+
 export const authentication = (app) => {
   const authentication = new AuthenticationService(app)
 
   authentication.register('jwt', new JWTStrategy())
   authentication.register('local', new LocalStrategy())
-  authentication.register('google', new OAuthStrategy())
+  authentication.register('google', new GoogleStrategy())
 
   app.use('authentication', authentication)
   app.configure(oauth())
