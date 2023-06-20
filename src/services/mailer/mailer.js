e3307cd3642add79c50d3770c87c2324b3a884ef
gachagajnr <williampius17@users.noreply.github.com>
2023-06-20 12:08:36 +0300

    finish maier setup

--- src/services/mailer/mailer.js
+++ src/services/mailer/mailer.js
@@ -1,17 +1,42 @@
 import { MailerService, getOptions } from './mailer.class.js'
 import { mailerPath, mailerMethods } from './mailer.shared.js'
+import aMailer from 'feathers-mailer'
+import nodemailer  from'nodemailer'
 
 export * from './mailer.class.js'
 
 // A configure function that registers the service and its hooks via `app.configure`
-export const mailer = (app) => {
+export const mailer = async(app) => {
+  const account = await nodemailer.createTestAccount() // internet required
+
   // Register our service on the Feathers application
-  app.use(mailerPath, new MailerService(getOptions(app)), {
-    // A list of all methods this service exposes externally
-    methods: mailerMethods,
-    // You can add additional custom events to be sent to clients here
-    events: []
-  })
+  // app.use(mailerPath, new MailerService(getOptions(app)), {
+  //   // A list of all methods this service exposes externally
+  //   methods: mailerMethods,
+  //   // You can add additional custom events to be sent to clients here
+  //   events: []
+  // })
+  const transporter = {
+    host: account.smtp.host,
+    port: account.smtp.port,
+    secure: account.smtp.secure, // 487 only
+    requireTLS: true,
+    auth: {
+      user: account.user, // generated ethereal user
+      pass: account.pass // generated ethereal password
+    }
+  }
+
+  app.use('mailer', aMailer(transporter, { from: account.user }))
+
+  // Use the service
+  const email = {
+    to: 'president@mars.com',
+    subject: 'SMTP test',
+    html: 'This is the email body'
+  }
+
+  await app.service('mailer').create(email)
   // Initialize hooks
   app.service(mailerPath).hooks({
     around: {
